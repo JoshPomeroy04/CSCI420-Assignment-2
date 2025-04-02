a8f9b6d30150528963031ecad57bfe7b1b578267
Josh Pomeroy <joshuaming2004@gmail.com>
2025-04-02 16:27:15 -0400

    All 3 sets masked and flattened in csv file

--- mask.py
+++ mask.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from pygments.lexers import PythonLexer
+import csv
 
 FOLDER_PATH = "CSCI_420/CSCI420-Assignment-2"
 lexer = PythonLexer()
-csv_corpus = pd.read_csv(f'{FOLDER_PATH}/Archive/ft_test.csv')
+train_corpus = pd.read_csv(f'{FOLDER_PATH}/Archive/ft_train.csv')
+test_corpus = pd.read_csv(f'{FOLDER_PATH}/Archive/ft_test.csv')
+valid_corpus = pd.read_csv(f'{FOLDER_PATH}/Archive/ft_valid.csv')
 
 def join(tokens):
     """Custom join function to merge tokens after using Pygments tokenizer.
@@ -22,12 +25,23 @@ def join(tokens):
             joined += token + " "
     return joined
 
-target = csv_corpus["target_block"][0]
-method = csv_corpus["cleaned_method"][0]
-method_tokens = [t[1] for t in lexer.get_tokens(method) if t[1] != ' ']
-target_tokens = [t[1] for t in lexer.get_tokens(target) if t[1] != ' ']
+def mask(input_data, output_file):
+    with open(f"{FOLDER_PATH}/data/{output_file}", mode='w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Masked Method", "Target Code"])
 
-target = " ".join(target_tokens)
-method = join(method_tokens).replace("    ", "<TAB> ").replace(target, "<MASK> ", 1)
+        for i in range(0, len(input_data["cleaned_method"])):
+            method = input_data["cleaned_method"][i]
+            target = input_data["target_block"][i]
+            method_tokens = [t[1] for t in lexer.get_tokens(method) if t[1] != ' ']
+            target_tokens = [t[1] for t in lexer.get_tokens(target) if t[1] != ' ']
+            target = " ".join(target_tokens)
+            method = join(method_tokens).replace("    ", "<TAB> ").replace(target, "<MASK> ", 1).replace("\n", "")
+            csv_writer.writerow([method, target])
 
-print(method.replace("\n", ""))
+print("Masking training set")
+mask(train_corpus, "train_set.csv")
+print("Masking test set")
+mask(test_corpus, "test_set.csv")
+print("Masking valid set")
+mask(valid_corpus, "valid_set.csv")
